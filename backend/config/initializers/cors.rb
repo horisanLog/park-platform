2e449544788905555990e4a7fee68b74a5c8dc83
horisanLog <ttie7374@gmail.com>
2023-01-31 10:14:54 +0900

    init nginx

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ["http://localhost:3000", "http://localhost"]
+    origins ["http://localhost:3000", "http://localhost", "http://localhost/graphql"]
 
     resource "*",
       headers: :any,
